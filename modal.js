86f96382184dcb3b1f0bc798ceafa5cdcf67d240
Praded3 <125351697+Praded3@users.noreply.github.com>
2023-05-06 23:26:35 +0300

    few

--- modal.js
+++ modal.js
@@ -1,18 +1,30 @@
 const refs = {
     backdrop: document.querySelector('.backdrop'),
-    modalForm: document.activeElement('.modal__form'),
+    modalForm: document.querySelector('.modal__form'),
     modalFormBtn: document.querySelector('.modal__btn'),
-}
+};
 
 const openBtns = {
     heroBtn: document.querySelector('.hero__btn'),
     advantagesBtn: document.querySelector('.advantages__btn'),
     nationwideBtn: document.querySelector('.nationwide__btn'),
     questionBtn: document.querySelector('.question__btn'),
-}
+};
+openBtns.heroBtn.addEventListener('click', () => {
+    refs.backdrop.classList.toggle('is-hiden')
+});
+openBtns.advantagesBtn.addEventListener('click', () => {
+    refs.backdrop.classList.toggle('is-hiden')
+});
+openBtns.nationwideBtn.addEventListener('click', () => {
+    refs.backdrop.classList.toggle('is-hiden')
+});
+openBtns.questionBtn.addEventListener('click', () => {
+    refs.backdrop.classList.toggle('is-hiden')
+});
 
-function doModalOpen (btn) {
-    
-}
 
-console.log(refs.backdrop);
+openBtns.heroBtn.addEventListener('click', onOpenBtnsClick);
+
+
+
