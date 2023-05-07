c29924dd408d8479abf174f7a80b1b4c4cd63d6b
Praded3 <o.pradidenko@gmail.com>
2023-05-07 13:02:18 +0300

    dvs

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ const refs = {
     backdrop: document.querySelector('.backdrop'),
     modalForm: document.querySelector('.modal__form'),
     modalFormBtn: document.querySelector('.modal__btn'),
+    modalCloseBtn: document.querySelector('.modal__close-btn'),
 };
 
 const openBtns = {
@@ -10,21 +11,67 @@ const openBtns = {
     nationwideBtn: document.querySelector('.nationwide__btn'),
     questionBtn: document.querySelector('.question__btn'),
 };
-openBtns.heroBtn.addEventListener('click', () => {
-    refs.backdrop.classList.toggle('is-hiden')
-});
-openBtns.advantagesBtn.addEventListener('click', () => {
-    refs.backdrop.classList.toggle('is-hiden')
-});
-openBtns.nationwideBtn.addEventListener('click', () => {
-    refs.backdrop.classList.toggle('is-hiden')
-});
-openBtns.questionBtn.addEventListener('click', () => {
-    refs.backdrop.classList.toggle('is-hiden')
-});
+// openBtns.heroBtn.addEventListener('click', () => {
+//     refs.backdrop.classList.toggle('is-hiden')
+// });
+// openBtns.advantagesBtn.addEventListener('click', () => {
+//     refs.backdrop.classList.toggle('is-hiden')
+// });
+// openBtns.nationwideBtn.addEventListener('click', () => {
+//     refs.backdrop.classList.toggle('is-hiden')
+// });
+// openBtns.questionBtn.addEventListener('click', () => {
+//     refs.backdrop.classList.toggle('is-hiden')
+// });
+
+//==================== open & close modal ======================
+const doModalClose = () => {
+    refs.backdrop.classList.toggle('is-hiden');
+    openBtns.heroBtn.addEventListener('click', (visabilityBackdrop) );
+    openBtns.advantagesBtn.addEventListener('click', (visabilityBackdrop) );
+    openBtns.nationwideBtn.addEventListener('click', (visabilityBackdrop) );
+    openBtns.questionBtn.addEventListener('click', (visabilityBackdrop) );
+};
+
+refs.modalCloseBtn.addEventListener('click', (doModalClose) )
+
+const visabilityBackdrop = () => {
+    refs.backdrop.classList.toggle('is-hiden');
+    openBtns.heroBtn.removeEventListener('click', (visabilityBackdrop) );
+    openBtns.advantagesBtn.removeEventListener('click', (visabilityBackdrop) );
+    openBtns.nationwideBtn.removeEventListener('click', (visabilityBackdrop) );
+    openBtns.questionBtn.removeEventListener('click', (visabilityBackdrop) );
+
+};
+
+openBtns.heroBtn.addEventListener('click', (visabilityBackdrop) );
+openBtns.advantagesBtn.addEventListener('click', (visabilityBackdrop) );
+openBtns.nationwideBtn.addEventListener('click', (visabilityBackdrop) );
+openBtns.questionBtn.addEventListener('click', (visabilityBackdrop) );
+
+//==================== /open & close modal/ ======================
+
+const firstStepBtn = document.querySelector('.first-step__btn');
+const modaZip = document.querySelector('.modal-zip');
+ console.log(modaZip.value);
 
 
-openBtns.heroBtn.addEventListener('click', onOpenBtnsClick);
+const confirmFirstStep = () => {
+    
+}
 
 
+
+
+
+
+
+const qqq = document.querySelectorAll('.is-not-displayed');
+
+const www = function eee(array) {
+    array.forEach(element => {
+        element.classList.toggle('is-not-displayed');
+    });
+}
+www(qqq);
 
